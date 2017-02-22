3eacda384a41174a4e28d4736447c0408ad6d4fb
cperera <cperera90+shackday@gmail.com>
2017-02-21 21:03:17 -0800

    commented homophones script

--- src/python/homophones.py
+++ src/python/homophones.py
@@ -1,13 +1,23 @@
 import sys
+import json
 import pronouncing
+
+"""
+@param: word, a lowercase string
+@returns: a list of homophones for word
+"""
 
 def homophones(word):
     phoneList = pronouncing.phones_for_word(word)
     results = []
     for phone in phoneList:
+        # pronouncing.search uses pronouncing to search for a RegEx of sounds
         results.extend(pronouncing.search("^" + phone + "$"))
     return results
-
+"""
+Test cases:
+bear, two, pear
+"""
 def main():
     print("BEAR:")
     for i in homophones("bear"):
@@ -19,11 +29,18 @@ def main():
     for i in homophones("pear"):
         print(i)
 
+"""
+if this script is called with arguments, return JSON for each arg's homophones
+{ arg1 : ["homophone1","homophone2","homophone3",
+arg2 : ["homophone1","homophone2"]
+
+"""
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        print("{")
         for arg in sys.argv[1:]:
-            print("{" + arg + ":")
-            print(homophones(arg))
+            print(json.dumps(arg) + ":")
+            print(json.dumps(homophones(arg)))
             print(",")
         print("}")
     else:
