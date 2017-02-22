44a220a862d0138bec506126d6727eb5ce8d637e
Chris Reyes <ChrisReyes@alum.mit.edu>
2017-02-21 21:20:06 -0800

    Partial commit towards connecting with the backend

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Pun Inspiration Generator
 See Taiga.io for backlog: https://tree.taiga.io/project/creyes17-pun-it-squares/
 
 To see the HTML interface, simply open the `src/html/index.html` file in a web browser.
+
+To use the HTML interface, you'll need to start up a webserver. Navigate to the `python` directory and use the command:
+
+    python -m SimpleHTTPServer
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,7 +2,30 @@
 let PunIt = function() {
 	return {
 		/**
-		 * Puts the given text in the given output by overwriting it's contents
+		 * Gets the value of the specified input and replaces output with a list of relevant puns
+		 *
+		 * @param input: The ID of a text input dom element
+		 * @param output: The ID of a dom element to replace with the results
+		 *
+		 * Side effects: Modifies the dom element with an ID of output
+		 */
+		putPunsFromInputToOutput: function(input, output) {
+			let word = $("#" + input).val();
+
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:8000/homophones.py",
+				data: {
+					param: word
+				}
+			}).done(
+				function(results) {
+					$("#" + output).text(results);
+				}
+			);
+		},
+		/**
+		 * Puts the specified text in the specified output dom element.
 		 *
 		 * @param output: The ID of an HTML element to override
 		 * @returns: nothing
@@ -14,8 +37,6 @@ let PunIt = function() {
 		},
 
 		/**
-		 * findPunsForTextInput
-		 *
 		 * Gets all puns for the given text input field identified with the given id
 		 *
 		 * @param id: The id of an input field with a value of a word for which to find puns
@@ -26,15 +47,26 @@ let PunIt = function() {
 		},
 
 		/**
-		 * findPuns
-		 *
 		 * Gets all puns for the given word
 		 *
 		 * @param word: The word for which to find puns
 		 * @returns: A list of words
 		 */
 		findPuns : function(word) {
-			return [word, word, word];
+			let puns = [];
+			$.ajax({
+				type: "POST",
+				url: "../python/homophones.py",
+				data: {
+					param: word
+				}
+			}).done(
+				function(results) {
+					puns = results;
+					alert(puns);
+				}
+			);
+			return puns;
 		}
 	};
 }();
